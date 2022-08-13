93c189de669faccdf1a63370d541258c824ef06a
tejaskhartude <tejaskhartude@gmail.com>
2022-08-13 08:17:33 +0530

    Added Valid Pallindrome problem

--- src/Arrays/ValidPallindrome.java
+++ src/Arrays/ValidPallindrome.java
@@ -1,5 +1,9 @@
 package Arrays;
 
+//A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+// it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+//
+//Given a string s, return true if it is a palindrome, or false otherwise.
 public class ValidPallindrome {
 
     public boolean isPallindrome(String s) {
