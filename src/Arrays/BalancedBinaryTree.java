acddf45960fb68654817578dedfa23db3cfce9ff
tejaskhartude <tejaskhartude@gmail.com>
2022-08-13 17:59:08 +0530

    Added Balanced Binary Tree

--- src/Arrays/BalancedBinaryTree.java
+++ src/Arrays/BalancedBinaryTree.java
@@ -1,5 +1,10 @@
 package Arrays;
 
+//Given a binary tree, determine if it is height-balanced.
+//
+//For this problem, a height-balanced binary tree is defined as:
+//
+//a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
 public class BalancedBinaryTree {
 
     public boolean isBalanced(TreeNode root) {
