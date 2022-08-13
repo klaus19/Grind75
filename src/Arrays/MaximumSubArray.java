c28f9cc2a3a31bbf245869e8396ad2484459162f
tejaskhartude <tejaskhartude@gmail.com>
2022-08-13 16:13:33 +0530

    Added MaximumSubArray with O(n) tIME COMPLEXITY and O(1) space complexity

--- src/Arrays/MaximumSubArray.java
+++ src/Arrays/MaximumSubArray.java
@@ -5,19 +5,18 @@ package Arrays;
 
 public class MaximumSubArray {
 
-    static int findSum(int nums[]){
-        int curr_sum = 0;
-        int max_sum=Integer.MIN_VALUE;
+    public int findSum(int nums[]){
 
-        for (int i=0;i<nums.length;i++)
+        int curr_sum=nums[0];
+        int max_sum =curr_sum;
+
+
+        for (int i=1;i<nums.length;i++)
         {
-            curr_sum +=nums[i];
-            if (max_sum<curr_sum){
-                max_sum=curr_sum;
-                if (curr_sum<0){
-                    curr_sum=0;
-                }
-            }
+           curr_sum = Math.max(curr_sum+nums[i],nums[i]);
+           max_sum= Math.max(max_sum,curr_sum);
+
+
 
 
         }
@@ -26,7 +25,7 @@ public class MaximumSubArray {
 
     public static void main(String[] args) {
         MaximumSubArray max = new MaximumSubArray();
-        int[]nums = new int[]{-2, -3, 4, -1, -2, 1, 5, -3};
+        int[]nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
         System.out.println(max.findSum(nums));
     }
 }
