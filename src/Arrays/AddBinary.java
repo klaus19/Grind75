944a765c0295cae13034def606b1e0b8d5d569c8
tejaskhartude <tejaskhartude@gmail.com>
2022-08-16 12:31:18 +0530

    Add binary

--- src/Arrays/AddBinary.java
+++ src/Arrays/AddBinary.java
@@ -1,10 +1,15 @@
 package Arrays;
 
+
+//
+
 public class AddBinary {
 
     public String addBinary(String a, String b) {
 
-        return Integer.toBinaryString(Integer.parseInt(a,2)+Integer.parseInt(b,2));
+        return Integer.toBinaryString(Integer.parseInt(a,2)+Integer.parseInt(b,2));  //not optimal solution with O(N+M)
+
+
 
     }
 }
