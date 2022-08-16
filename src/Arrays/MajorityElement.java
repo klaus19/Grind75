3d8805a8c61777aa96900c37b2681fcf498c82bb
tejaskhartude <tejaskhartude@gmail.com>
2022-08-16 10:45:02 +0530

    Added Reverse LinkedlIST

---
+++ src/Arrays/MajorityElement.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    public int countMajority(int[]nums){
+
+        Map<Integer,Integer>map = new HashMap<>();
+        int count =0;
+
+        for (int i=0;i<nums.length;i++){
+            if (!map.containsKey(nums[i])){
+                map.put(nums[i],count+1);
+            }else{
+                return map.get(count);
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        MajorityElement met = new MajorityElement();
+        int[]ans = new int[]{3,2,3};
+        System.out.println(met.countMajority(ans));
+    }
+}
